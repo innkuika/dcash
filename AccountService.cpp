e2154cf4b0653d1c48c281d9e5ebdd7d64a44cd3
innkuika <innku.ika@gmail.com>
2021-05-31 03:37:41 -0700

    transfer

--- AccountService.cpp
+++ AccountService.cpp
@@ -22,13 +22,100 @@ using namespace std;
 using namespace rapidjson;
 
 AccountService::AccountService() : HttpService("/users") {
-  
+}
+
+void respond_email_and_balance(string email, int balance, int statusCode, HTTPResponse *response) {
+    // use rapidjson to create a return object
+    Document document;
+    Document::AllocatorType &a = document.GetAllocator();
+    Value o;
+    o.SetObject();
+
+    // add a key value pair directly to the object
+    o.AddMember("email", email, a);
+    o.AddMember("balance", balance, a);
+
+    // now some rapidjson boilerplate for converting the JSON object to a string
+    document.Swap(o);
+    StringBuffer buffer;
+    PrettyWriter<StringBuffer> writer(buffer);
+    document.Accept(writer);
+
+    // set the return object
+    response->setStatus(statusCode);
+    response->setContentType("application/json");
+    response->setBody(buffer.GetString() + string("\n"));
 }
 
 void AccountService::get(HTTPRequest *request, HTTPResponse *response) {
+    User *user;
+    try {
+        user = getAuthenticatedUser(request);
+    } catch (const ClientError &error) {
+        response->setStatus(error.status_code);
+    }
 
+    // check if userid is in body
+    if (request->getPathComponents().size() != 2) {
+        // userid is not in body or there are more than 2 path components
+        response->setStatus(400);
+        return;
+    }
+
+    // check if user is in db
+    if (m_db->users.count(user->username) == 0) {
+        // user not in db
+        response->setStatus(404);
+        return;
+    }
+
+    // check if user and the user id passed in points to the same user
+    string user_id = request->getPathComponents()[1];
+    if (user->user_id != user_id) {
+        response->setStatus(403);
+        return;
+    }
+
+    respond_email_and_balance(user->email, user->balance, 200, response);
 }
 
 void AccountService::put(HTTPRequest *request, HTTPResponse *response) {
+    User *user;
+    try {
+        user = getAuthenticatedUser(request);
+    } catch (const ClientError &error) {
+        response->setStatus(error.status_code);
+    }
 
+    // check if userid is in body
+    if (request->getPathComponents().size() != 2) {
+        // userid is not in body or there are more than 2 path components
+        response->setStatus(400);
+        return;
+    }
+
+    // check if user is in db
+    if (m_db->users.count(user->username) == 0) {
+        // user not in db
+        response->setStatus(404);
+        return;
+    }
+
+    // check if user and the user id passed in points to the same user
+    string user_id = request->getPathComponents()[1];
+    if (user->user_id != user_id) {
+        response->setStatus(403);
+        return;
+    }
+
+    WwwFormEncodedDict args = request->formEncodedBody();
+    try {
+        user->email = args.get("email");
+    } catch (...) {
+        // email was not passed in
+        response->setStatus(400);
+        return;
+    }
+
+    respond_email_and_balance(user->email, user->balance, 200, response);
 }
--- TransferService.cpp
+++ TransferService.cpp
@@ -23,7 +23,77 @@ using namespace std;
 
 TransferService::TransferService() : HttpService("/transfers") { }
 
+void respond_balance_and_transfers(vector<Transfer *> transfers, int balance, string from_username,  int statusCode, HTTPResponse *response) {
+    // use rapidjson to create a return object
+    Document document;
+    Document::AllocatorType &a = document.GetAllocator();
+    Value o;
+    o.SetObject();
 
+    // add a key value pair directly to the object
+    o.AddMember("balance", balance, a);
+
+    // create an array
+    Value array;
+    array.SetArray();
+
+    for(int i = 0; i < transfers.size(); i++) {
+        if (transfers[i]->to->username == from_username || transfers[i]->from->username == from_username) {
+            // add an object to our array
+            Value to;
+            to.SetObject();
+            to.AddMember("from", transfers[i]->from->username, a);
+            to.AddMember("to", transfers[i]->to->username, a);
+            to.AddMember("amount", std::to_string(transfers[i]->amount), a);
+            array.PushBack(to, a);
+        }
+    }
+
+    o.AddMember("array_key", array, a);
+    // now some rapidjson boilerplate for converting the JSON object to a string
+    document.Swap(o);
+    StringBuffer buffer;
+    PrettyWriter<StringBuffer> writer(buffer);
+    document.Accept(writer);
+
+    // set the return object
+    response->setStatus(statusCode);
+    response->setContentType("application/json");
+    response->setBody(buffer.GetString() + string("\n"));
+}
 void TransferService::post(HTTPRequest *request, HTTPResponse *response) {
+    User *from_user;
+    try {
+        from_user = getAuthenticatedUser(request);
+    } catch (const ClientError &error) {
+        response->setStatus(error.status_code);
+    }
 
+    WwwFormEncodedDict args = request->formEncodedBody();
+    string to_username = args.get("to");
+    int amount = std::stoi(args.get("amount"));
+
+    // check if to_username exit in db
+    if (m_db->users.count(to_username) == 0) {
+        // user not in db
+        response->setStatus(404);
+        return;
+    }
+    // check if balance > amount
+    if (amount < from_user->balance) {
+        // doesn't have enough money
+        response->setStatus(403);
+        return;
+    }
+    User *to_user = m_db->users[to_username];
+    from_user->balance -= amount;
+    to_user += amount;
+
+    // add this transaction to db
+    Transfer *transfer = new Transfer();
+    transfer->from = from_user;
+    transfer->to = to_user;
+    transfer->amount = amount;
+    m_db->transfers.push_back(transfer);
+    respond_balance_and_transfers(m_db->transfers, from_user->balance, from_user->username, 200, response);
 }
